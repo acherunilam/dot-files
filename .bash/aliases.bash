c51638366d4a0018cb093cdd4d4df8fa78d16a87
Adithya Cherunilam <me@adi.xyz>
2015-08-05 00:51:13 +0530

    [bash] sanitize the aliases

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -36,24 +36,33 @@ alias x=extract
 
 # list of functions
 
+ctc() {
+  if hash pygmentize 2>/dev/null ; then
+    prog='pygmentize -g'
+  else
+    prog='cat'
+  fi
+  sed -e 's/#.*$//' -e '/^$/d' $1 | $prog
+}
+
 extract() {
   if [ -f "$1" ] ; then
-  case "$1" in
-    *.tar.bz2) tar vxjf "$1" ;;
-    *.tar.gz) tar vxzf "$1" ;;
-    *.tar.Z) tar vxzf "$1" ;;
-    *.bz2) bunzip2 -v "$1" ;;
-    *.rar) 7z x "$1" ;;
-    *.gz) gunzip -v "$1" ;;
-    *.jar) 7z x "$1" ;;
-    *.tar) tar vxf "$1" ;;
-    *.tar.xz) tar vxf "$1" ;;
-    *.tbz2) tar vxjf "$1" ;;
-    *.tgz) tar vxzf "$1" ;;
-    *.zip) 7z x "$1" ;;
-    *.Z) uncompress -v "$1" ;;
-    *.7z) 7z x "$1" ;;
-    *) echo "'$1' cannot be extracted" ;;
+    case "$1" in
+      *.tar.bz2) tar vxjf "$1" ;;
+      *.tar.gz) tar vxzf "$1" ;;
+      *.tar.Z) tar vxzf "$1" ;;
+      *.bz2) bunzip2 -v "$1" ;;
+      *.rar) 7z x "$1" ;;
+      *.gz) gunzip -v "$1" ;;
+      *.jar) 7z x "$1" ;;
+      *.tar) tar vxf "$1" ;;
+      *.tar.xz) tar vxf "$1" ;;
+      *.tbz2) tar vxjf "$1" ;;
+      *.tgz) tar vxzf "$1" ;;
+      *.zip) 7z x "$1" ;;
+      *.Z) uncompress -v "$1" ;;
+      *.7z) 7z x "$1" ;;
+      *) echo "'$1' cannot be extracted" ;;
     esac
   else
     echo "'$1' is not a file"
@@ -64,10 +73,19 @@ ff() {
   find . -type f -iname '*'"$*"'*' -ls 2>/dev/null
 }
 
+ipp() {
+  netstat -i | tail -n+3 | awk '{print $1'} | grep -v "lo" | while read interface ; do
+    ip=$(ifconfig $interface | grep "inet[^6]" | awk '{print $2}' | cut -d':' -f2)
+    if [ -n "$ip" ] ; then
+      echo -e "$interface\t$ip"
+    fi
+  done
+}
+
 share() {
   temp="/tmp/myshare-$RANDOM"
   mkdir -p $temp &>/dev/null
-  for f in "$@"; do
+  for f in "$@" ; do
     path=$(realpath "$f")
     name=$(basename "$f")
     ln -s "$path" "$temp/$name"
@@ -76,42 +94,26 @@ share() {
 
   port=8000
   result=$(netstat -tulpn 2>/dev/null | grep "$port")
-  while [ -n "$result" ]
-  do
+  while [ -n "$result" ] ; do
     if [ -n "$result" ] ; then
       ((port++))
-          fi
-          result=$(netstat -tulpn 2>/dev/null | grep "$port")
+    fi
+      result=$(netstat -tulpn 2>/dev/null | grep "$port")
   done
 
   netstat -i | tail -n+3 | awk '{print $1}' | grep -v "lo" >$temp/interfaces && echo lo >>$temp/interfaces
-        cat interfaces | while read interface
-        do
-                ip=$(ifconfig $interface | grep "inet[^6]" | awk '{print $2}' | cut -c 6-)
-                if [ -n "$ip" ] ; then
+  cat interfaces | while read interface ; do
+    ip=$(ifconfig $interface | grep "inet[^6]" | awk '{print $2}' | cut -c 6-)
+    if [ -n "$ip" ] ; then
       echo "Starting server at http://$ip:$port/ ..."
-                  break
-                fi
-        done
+      break
+    fi
+  done
   rm interfaces &>/dev/null
 
   python2 -m SimpleHTTPServer $port 2>/dev/null
 
   cd - &>/dev/null
   rm -rf $temp &>/dev/null
-}
-
-ipp() {
-        netstat -i | tail -n+3 | awk '{print $1'} | grep -v "lo" | while read interface
-        do
-                ip=$(ifconfig $interface | grep "inet[^6]" | awk '{print $2}' | cut -d':' -f2)
-                if [ -n "$ip" ] ; then
-                        echo -e "$interface\t$ip"
-                fi
-        done
-}
-
-ctc() {
-  sed -e 's/#.*$//' -e '/^$/d' $1 | pygmentize -g
 }
 
