6ee24e55b19b10aaa79f816650ccb5dc58a61312
Adithya Cherunilam <me@adi.xyz>
2016-12-10 22:49:11 -0500

    [bash] add alias for Sublime Text

--- .bash/mac.bash
+++ .bash/mac.bash
@@ -1,5 +1,7 @@
 # all of this is specific to just macOS
 
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+
 # send notification and make a sound
 # requires executable from https://github.com/julienXX/terminal-notifier
 notify() {
