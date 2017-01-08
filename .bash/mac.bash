ec8c12f4ffe281db6a786a46367cc95f4e980388
Adithya Cherunilam <me@adi.xyz>
2017-01-07 23:39:28 -0500

    [bash] add helper for cd-ing into an open Finder window

--- .bash/mac.bash
+++ .bash/mac.bash
@@ -1,6 +1,16 @@
-# all of this is specific to just macOS
+alias slp='pmset displaysleepnow'                                                         # switch off display and go to sleep
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'              # open file with Sublime Text
 
-alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+# change directory to the one open in Finder
+cdf() {
+  target=$(osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)' 2>/dev/null)
+  if [[ -n $target ]] ; then
+    cd "$target"
+  else
+    echo "No Finder window found" >&2
+    return 2
+  fi
+}
 
 # send notification and make a sound
 # requires executable from https://github.com/julienXX/terminal-notifier
@@ -10,7 +20,8 @@ notify() {
   fi
 }
 
-# remove quarantine flag from this directory
+# remove extended attributes for a file downloaded from the internet
 whitelist() {
-  sudo xattr -rd com.apple.quarantine "$*"
+  sudo xattr -rd com.apple.metadata:kMDItemWhereFroms "$@"
+  sudo xattr -rd com.apple.quarantine "$@"
 }
