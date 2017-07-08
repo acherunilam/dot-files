890f3b034c26bdd782c8b06c54fa411b69aec6e4
Adithya Cherunilam <me@adi.xyz>
2017-07-08 18:53:11 +0530

    [setup] allow installation of specific dot files

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Dot-files for Bash, Git, Vim, etc.
 ## Setup
 ```bash
 git clone --recursive git@github.com:acherunilam/dot-files.git
-./dot-files/setup.sh      # backs up the existing dot files and installs the ones in this repo
+./dot-files/setup.sh --all   # backs up the existing dot files and installs the ones in this repo
 ```
--- setup.sh
+++ setup.sh
@@ -1,10 +1,101 @@
 #!/usr/bin/env bash
 
-SOURCE=$(dirname "$0")
+BASENAME=$(basename "$0")
+USAGE="Usage: $BASENAME [OPTIONS]
+A wrapper script to install the dot files present in this repo. Backups for
+existing dot files will be taken prior to copying this over.
+
+-h, --help            print this help
+-a, --all             install all dot files
+-e, --existing        skip file if it already exists
+--bash                install the bash dot files
+--editline            install the editline config file
+--fasd                install the fasd config file
+--git                 install the git config file
+--readline            install the readline config file
+--python              install the python config file
+--screen              install the screen config file
+--ssh                 install the ssh config file
+--tmux                install the tmux config file
+--vim                 install the vim config files
+"
+
+print_usage_and_exit() {
+  echo "$USAGE"
+  exit ${1:-0}
+}
+
+for arg in "$@" ; do
+  case "$arg" in
+    --help)
+      HELP=1;;
+    --all)
+      INSTALL_ALL=1;;
+    --existing)
+      SKIP_EXISTING=1;;
+    --bash)
+      INSTALL_BASH=1;;
+    --editline)
+      INSTALL_EDITLINE=1;;
+    --fasd)
+      INSTALL_FASD=1;;
+    --git)
+      INSTALL_GIT=1;;
+    --readline)
+      INSTALL_READLINE=1;;
+    --python)
+      INSTALL_PYTHON=1;;
+    --screen)
+      INSTALL_SCREEN=1;;
+    --ssh)
+      INSTALL_SSH=1;;
+    --tmux)
+      INSTALL_TMUX=1;;
+    --vim)
+      INSTALL_VIM=1;;
+    -[!-]*)
+      OPTIND=1
+      while getopts ":hae" short_arg $arg ; do
+        case "$short_arg" in
+          h)
+            HELP=1;;
+          a)
+            INSTALL_ALL=1;;
+          e)
+            SKIP_EXISTING=1;;
+          *)
+            print_usage_and_exit 1;;
+        esac
+      done;;
+    *)
+      print_usage_and_exit 1;;
+  esac
+done
+
+[[ $HELP == 1 || -z "$@" ]] && print_usage_and_exit
+
+
+SRC_DIR=$(dirname "$0")
+SOURCE=""
 TARGET="$HOME"
-rsync -avzh --backup --suffix=.bak --exclude={*.swp,.git,README.md,setup.sh} "$SOURCE/" "$TARGET"
+EXCLUDE_FILES='--exclude={*.swp,.git,README.md,setup.sh}'
+[[ "$OSTYPE" != "darwin"* ]]  && EXCLUDE_FILES+=' --exclude=mac.bash'
+[[ $SKIP_EXISTING == 1 ]]     && OVERWRITE_SETTINGS='--ignore-existing' || OVERWRITE_SETTINGS='--backup --suffix=.bak'
+[[ $INSTALL_ALL == 1 ]]       && SOURCE+=" $SRC_DIR/"
+[[ $INSTALL_BASH == 1 ]]      && SOURCE+=" $SRC_DIR/{.bash,.bashrc,.bash_profile}"
+[[ $INSTALL_EDITLINE == 1 ]]  && SOURCE+=" $SRC_DIR/.editrc"
+[[ $INSTALL_FASD == 1 ]]      && SOURCE+=" $SRC_DIR/.fasdrc"
+[[ $INSTALL_GIT == 1 ]]       && SOURCE+=" $SRC_DIR/.gitconfig"
+[[ $INSTALL_READLINE == 1 ]]  && SOURCE+=" $SRC_DIR/.inputrc"
+[[ $INSTALL_PYTHON == 1 ]]    && SOURCE+=" $SRC_DIR/.pythonrc"
+[[ $INSTALL_SCREEN == 1 ]]    && SOURCE+=" $SRC_DIR/.screenrc"
+[[ $INSTALL_SSH == 1 ]]       && SOURCE+=" $SRC_DIR/.ssh"
+[[ $INSTALL_TMUX == 1 ]]      && SOURCE+=" $SRC_DIR/.tmux.conf"
+[[ $INSTALL_VIM == 1 ]]       && SOURCE+=" $SRC_DIR/{.vim,.vimrc}"
 
-chmod 700 "$TARGET" "$TARGET/.ssh"
-chmod 644 "$TARGET/.ssh/config"
+rsync -avzh $OVERWRITE_SETTINGS $(eval "echo $EXCLUDE_FILES") $(eval "echo $SOURCE") "$TARGET"
 
-[[ "$OSTYPE" != "darwin"* ]] && rm "$TARGET/.bash/mac.bash"
+if [[ $INSTALL_ALL == 1 || $INSTALL_SSH == 1 ]] ; then
+  chmod 700 "$TARGET" "$TARGET/.ssh"
+  chmod 644 "$TARGET/.ssh/config"
+fi
