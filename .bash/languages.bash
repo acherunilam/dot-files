f56410971de21232869af02e164cb214e177628b
Adithya Cherunilam <me@adi.xyz>
2020-06-19 22:22:35 -0700

    [bash] add support for Perl

--- .bash/languages.bash
+++ .bash/languages.bash
@@ -17,6 +17,14 @@ elif [[ "$OSTYPE" == "darwin"* ]] ; then
     export JAVA_HOME=$(/usr/libexec/java_home)
 fi
 
+# Perl
+# PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
+export PATH="$HOME/perl5/bin${PATH:+:${PATH}}"
+export PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+export PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+export PERL_MB_OPT="--install_base \"$HOME/perl5\""
+export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
+
 # Python
 export PYTHONSTARTUP="$HOME/.pythonrc"
 if [[ "$OSTYPE" == "linux"* ]] ; then
