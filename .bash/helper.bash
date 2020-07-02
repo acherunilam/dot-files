d80984379b9773fedc717d11a9e3496e605527eb
Adithya Cherunilam <me@adi.xyz>
2020-07-02 02:22:47 -0700

    [bash] update extract(), drop support for extracting DMG because it's buggy

--- .bash/helper.bash
+++ .bash/helper.bash
@@ -148,7 +148,6 @@ extract() {
             *.7z)       7z x "$1"               ;;
             *.bz2)      bunzip2 "$1"            ;;
             *.deb)      ar x "$1"               ;;
-            *.dmg)      7z x "$1"               ;;
             *.exe)      cabextract "$1"         ;;
             *.gz)       gunzip "$1"             ;;
             *.jar)      7z x "$1"               ;;
