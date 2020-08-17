0ecd35ac243aafc02c9f104bc8c350f4ec12fa0f
Adithya Cherunilam <me@adi.xyz>
2020-08-17 00:33:54 -0700

    [bash] load ACME environment only if present

--- .bash/acme.bash
+++ .bash/acme.bash
@@ -1,3 +1,3 @@
 # load ACME client to renew Let's Encrypt certs
 # requires executable from https://github.com/acmesh-official/acme.sh
-source "$HOME/.acme.sh/acme.sh.env"
+[[ -f "$HOME/.acme.sh/acme.sh.env" ]] && source "$HOME/.acme.sh/acme.sh.env"
