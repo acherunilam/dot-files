047f9e15104af0141e11e07ddd77d9522ffc18d0
Adithya Cherunilam <me@adi.xyz>
2021-09-27 19:20:57 -0700

    [setup] bug fix, exclusion now works

--- setup.sh
+++ setup.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2086
 
-"""
-Requires additional dependencies:
 
-sudo dnf install -y curl fzf rsync tmux vim
-sudo curl -sS https://raw.githubusercontent.com/clvv/fasd/master/fasd -o /usr/local/bin/fasd
-"""
-
-function print_usage_and_exit() {
-    echo "$USAGE"
-    exit ${1:-0}
-}
-
-function check_if_installed() {
-    if ! type -P "$1" 1>/dev/null ; then
-        "'$1' is not installed" >&2
-        exit 1
-    fi
-}
+# Set up dot files.
+#
+# Dependencies:
+#       sudo dnf install -y curl rsync vim
+#       sudo curl -sS https://raw.githubusercontent.com/clvv/fasd/master/fasd -o /usr/local/bin/fasd
 
 
 BASENAME=$(basename "$0")
+TARGET_DIR="$HOME"
 USAGE="Usage: $BASENAME [OPTIONS]
 A wrapper script to install the dot files present in this repo. Backups for
 existing dot files will be taken prior to copying this over.
@@ -38,8 +28,22 @@ existing dot files will be taken prior to copying this over.
 --skip-existing     skip installing the dot file if it already exists locally
 --ssh               install the ssh config file
 --tmux              install the tmux config file
---vim               install the vim config files
-"
+--vim               install the vim config files"
+
+
+function print_usage_and_exit() {
+    echo "$USAGE"
+    exit ${1:-0}
+}
+
+function check_if_installed() {
+    if ! type -P "$1" 1>/dev/null ; then
+        "$BASENAME: '$1' is not installed" >&2
+        exit 1
+    fi
+}
+
+
 for arg in "$@"; do
     case "$arg" in
     --all)
@@ -85,17 +89,18 @@ for arg in "$@"; do
         INSTALL_VIM=1
         ;;
     *)
-        print_usage_and_exit 1
+        print_usage_and_exit 1 >&2
         ;;
     esac
 done
-[[ $HELP == 1 || "$#" -eq 0 ]] && print_usage_and_exit
+[[ $# -eq 0 ]] && print_usage_and_exit 1 >&2
+[[ $HELP == 1 ]] && print_usage_and_exit
 
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 command git submodule update --init --recursive
 SOURCE=""
-EXCLUDE_FILES="--exclude ./.bash/fzf.bindings.bash --exclude ./.bash/fzf.completion.bash"
+EXCLUDE_FILES="--exclude=fzf.bindings.bash --exclude=fzf.completion.bash"
 [[ $SKIP_EXISTING == 1 ]] && OVERWRITE_SETTINGS="--ignore-existing" || OVERWRITE_SETTINGS="--backup --suffix=.bak"
 if [[ $INSTALL_ALL == 1 ]] ; then
     INSTALL_BASH=1
@@ -113,10 +118,7 @@ fi
 [[ $INSTALL_BASH == 1 ]] && SOURCE+=" ./.bashrc ./.bash_profile ./.bash/*.bash"
 [[ $INSTALL_EDITLINE == 1 ]] && SOURCE+=" ./.editrc"
 [[ $INSTALL_FASD == 1 ]] && SOURCE+=" ./.fasdrc"
-if [[ $INSTALL_FZF == 1 ]] ; then
-    check_if_installed "fzf"
-    EXCLUDE_FILES=""
-fi
+[[ $INSTALL_FZF == 1 ]] && EXCLUDE_FILES=""
 [[ $INSTALL_GIT == 1 ]] && SOURCE+=" ./.gitconfig"
 [[ $INSTALL_READLINE == 1 ]] && SOURCE+=" ./.inputrc"
 [[ $INSTALL_RIPGREP == 1 ]] && SOURCE+=" ./.ripgreprc"
@@ -124,19 +126,21 @@ fi
 [[ $INSTALL_SSH == 1 ]] && SOURCE+=" ./.ssh"
 [[ $INSTALL_TMUX == 1 ]] && SOURCE+=" ./.tmux.conf"
 [[ $INSTALL_VIM == 1 ]] && SOURCE+=" ./.vimrc"
-[[ "$OSTYPE" != "darwin"* ]] && EXCLUDE_FILES+=" --exclude=./.bash/mac.bash"
+[[ "$OSTYPE" != "darwin"* ]] && EXCLUDE_FILES+=" --exclude=mac.bash"
 check_if_installed "rsync"
-command rsync -avzh --copy-links --relative $OVERWRITE_SETTINGS $EXCLUDE_FILES $SOURCE "$HOME"
+command rsync -avhLK --relative $OVERWRITE_SETTINGS $EXCLUDE_FILES $SOURCE "$TARGET_DIR"
 
 
 if [[ $INSTALL_SSH == 1 ]]; then
-    command chmod 700 "$HOME"
-    command chmod 700 "$HOME/.ssh"
-    command chmod 644 "$HOME/.ssh/config"
+    command chmod 700 "$TARGET_DIR"
+    command chmod 700 "$TARGET_DIR/.ssh"
+    command chmod 644 "$TARGET_DIR/.ssh/config"
 fi
+
+
 if [[ $INSTALL_VIM == 1 ]]; then
     check_if_installed "curl"
-    command curl --silent -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    command curl --silent -fLo "$TARGET_DIR/.vim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
     check_if_installed "vim"
     command vim +PlugInstall +qall
 fi
