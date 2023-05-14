68e196e13484e81246fb7c67d9eb2bdab609ac7e
Adithya Cherunilam <me@adi.xyz>
2023-05-13 22:43:34 -0700

    [bash] update pipp(), migrate from OpenDNS to Google

--- .bash/helper.sh
+++ .bash/helper.sh
@@ -313,9 +313,9 @@ pbcopy() {
 #
 # shellcheck disable=SC2086
 pipp() {
-    local DIG_OPTS="+short +timeout=1 +retry=1 myip.opendns.com @resolver1.opendns.com"
-    command dig -4 A $DIG_OPTS | command grep -v '^;;'
-    command dig -6 AAAA $DIG_OPTS | command grep -v '^;;'
+    local DIG_OPTS="+short +timeout=1 +retry=1 TXT o-o.myaddr.l.google.com @ns1.google.com"
+    { command dig -4 $DIG_OPTS ; command dig -6 $DIG_OPTS ; } \
+        | command sed 's/"//g;/^;;/d'
 }
 
 # Upload contents to Sprunge, a public pastebin. If no input is passed, then the
