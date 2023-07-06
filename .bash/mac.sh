d2ecf56a0091f8d199aa9541f18783cfc75cc7b2
Adithya Cherunilam <me@adi.xyz>
2023-07-06 16:27:59 -0700

    [bash] add helper for inverting colors in an image

--- .bash/mac.sh
+++ .bash/mac.sh
@@ -61,6 +61,7 @@ alias tac='gtac'                                                            # BS
 
 # Helpers.
 alias dark='brightness 0 2>/dev/null'                                       # set display brightness to 0
+alias inv='mogrify -channel RGB -negate'                                    # invert colors for the image
 alias lck='pmset displaysleepnow'                                           # switch off display
 alias loc='mdfind -name 2>/dev/null'                                        # search globally by file name
 alias shred='gshred -vfzu -n 10'                                            # securely erase the file
