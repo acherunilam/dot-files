84458fa2c192851183ca55df9225e7b44d1b41ad
Adithya Cherunilam <me@adi.xyz>
2024-02-25 08:36:53 -0800

    [bootstrap] [golang] add yq

--- bootstrap/golang.sh
+++ bootstrap/golang.sh
@@ -6,6 +6,7 @@ PKGS=(
 	github.com/hakluke/haktrails
 	github.com/ipinfo/cli/ipinfo
 	github.com/ipinfo/mmdbctl
+	github.com/mikefarah/yq/v4
 	mvdan.cc/sh/v3/cmd/shfmt
 )
 go install "${PKGS[@]/%/@latest}"
