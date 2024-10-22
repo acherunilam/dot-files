b318a581d585a4049d7e1358b298db4eebdf9c16
Adithya Cherunilam <me@adi.xyz>
2024-10-21 19:50:43 -0700

    [bootstrap] [fedora] configure a lot more settings

--- bootstrap/fedora.sh
+++ bootstrap/fedora.sh
@@ -4,7 +4,6 @@
 # Global variables
 ################################################################################
 
-# TODO: Ensure that it's not running as root.
 if command ps -e | command grep -Eq "Xorg|wayland"; then
 	HAS_GUI=1
 else
@@ -17,6 +16,84 @@ else
 fi
 
 ################################################################################
+# Config
+################################################################################
+
+# Host
+# sudo hostnamectl set-hostname "$HOST_NAME"
+# sudo useradd -m -G wheel "$USER_NAME"
+# sudo passwd -d root
+if sudo grep -q '^# %wheel[[:space:]]\+ALL=(ALL)[[:space:]]\+NOPASSWD: ALL' /etc/sudoers; then
+	sudo sed -i 's/^# %wheel[[:space:]]\+ALL=(ALL)[[:space:]]\+NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
+	sudo sed -i 's/^%wheel[[:space:]]\+ALL=(ALL)[[:space:]]\+ALL/# %wheel ALL=(ALL) ALL/g' /etc/sudoers
+fi
+
+# Cockpit
+sudo dnf remove cockpit*
+# DNF
+if ! grep -q '^max_parallel_downloads' /etc/dnf/dnf.conf; then
+	echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf
+fi
+# DNS
+sudo tee /etc/systemd/resolved.conf <<EOF
+[Resolve]
+DNS=8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::884
+FallbackDNS=
+Domains=~.
+#DNSSEC=no
+DNSOverTLS=yes
+#MulticastDNS=no
+#LLMNR=resolve
+Cache=yes
+#CacheFromLocalhost=no
+#DNSStubListener=yes
+#DNSStubListenerExtra=0.0.0.0
+#DNSStubListenerExtra=::1
+#ReadEtcHosts=yes
+#ResolveUnicastSingleLabel=no
+#StaleRetentionSec=0
+EOF
+sudo systemctl restart systemd-resolved
+# Firewall
+sudo systemctl disable firewalld
+# SSH
+sudo sed -i 's/^PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+echo "PrintLastLog No" | sudo tee /etc/ssh/sshd_config.d/silent-login.conf
+sudo systemctl daemon-reload
+sudo systemctl reload sshd
+# TCP
+sudo tee /etc/sysctl.d/98-tcp.conf <<EOF
+# allow TCP with buffers up to 64MB
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+# increase Linux autotuning TCP buffer limit to 32MB
+net.ipv4.tcp_rmem = 4096 87380 33554432
+net.ipv4.tcp_wmem = 4096 65536 33554432
+# recommended for hosts with jumbo frames enabled
+net.ipv4.tcp_mtu_probing=1
+# BBR
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+EOF
+sudo sysctl -p /etc/sysctl.d/98-tcp.conf
+
+# Docker
+[[ $UID -ne 0 ]] && sudo usermod -aG docker "$USER"
+[[ ! -r /etc/docker/daemon.json ]] && echo "{}" | sudo tee /etc/docker/daemon.json
+command jq '.["metrics-addr"] = "0.0.0.0:9323"' /etc/docker/daemon.json | sudo tee /etc/docker/daemon.json
+sudo systemctl reload docker
+# Tailscale
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | sudo tee /etc/NetworkManager/dispatcher.d/pre-up.d/50-tailscale
+sudo chmod 755 /etc/NetworkManager/dispatcher.d/pre-up.d/50-tailscale
+sudo /etc/NetworkManager/dispatcher.d/pre-up.d/50-tailscale
+if grep -q '^FLAGS=""' /etc/default/tailscaled; then
+	sudo sed -i 's/^FLAGS=""/FLAGS="--debug=0.0.0.0:1234"/g' /etc/default/tailscaled
+fi
+
+################################################################################
 # CLI
 ################################################################################
 
@@ -25,7 +102,7 @@ sudo rpm --import "https://download.docker.com/linux/fedora/gpg"
 sudo dnf config-manager --add-repo "https://download.docker.com/linux/fedora/docker-ce.repo"
 # Google Cloud CLI
 sudo rpm --import "https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg"
-sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo <<EOF
+sudo tee /etc/yum.repos.d/google-cloud-sdk.repo <<EOF
 [google-cloud-cli]
 name=Google Cloud CLI
 baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
@@ -151,7 +228,6 @@ SERVICES=(
 	tor
 )
 sudo systemctl enable --now "${SERVICES[@]}"
-sudo usermod -aG docker "$USER"
 
 ################################################################################
 # GUI
@@ -200,27 +276,3 @@ if [[ $HAS_NVIDIA -eq 1 ]]; then
 	# To account for a bug where autoremove might wrongly remove the package.
 	sudo dnf mark install akmod-nvidia
 fi
-
-################################################################################
-# Config
-################################################################################
-
-# sudo hostnamectl set-hostname "$HOST_NAME"
-if sudo grep -q '^# %wheel[[:space:]]\+ALL=(ALL)[[:space:]]\+NOPASSWD: ALL' /etc/sudoers; then
-	sudo sed -i 's/^# %wheel[[:space:]]\+ALL=(ALL)[[:space:]]\+NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
-	sudo sed -i 's/^%wheel[[:space:]]\+ALL=(ALL)[[:space:]]\+ALL/# %wheel ALL=(ALL) ALL/g' /etc/sudoers
-fi
-sudo systemctl disable firewalld
-sudo dnf remove cockpit*
-sudo sed -i 's/^PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-echo "PrintLastLog No" | sudo tee /etc/ssh/sshd_config.d/silent-login.conf
-sudo systemctl daemon-reload
-sudo systemctl reload sshd
-# sudo useradd -m -G wheel "$USER_NAME"
-# sudo passwd -d root
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
-printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | sudo tee /etc/NetworkManager/dispatcher.d/pre-up.d/50-tailscale
-sudo chmod 755 /etc/NetworkManager/dispatcher.d/pre-up.d/50-tailscale
-sudo /etc/NetworkManager/dispatcher.d/pre-up.d/50-tailscale
