2db1a129e329ea80ebee4a56d4e48d210a1ce1fe
Adithya Cherunilam <me@adi.xyz>
2025-01-23 15:21:31 -0800

    [bootstrap] [golang] add ffuf

--- bootstrap/lib/golang.sh
+++ bootstrap/lib/golang.sh
@@ -3,6 +3,7 @@
 MODULES=(
 	github.com/cemulus/crt
 	github.com/danielgatis/imgcat
+	github.com/ffuf/ffuf/v2
 	github.com/hakluke/haktrails
 	github.com/ipinfo/cli/ipinfo
 	github.com/ipinfo/mmdbctl
